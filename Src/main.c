99c8e5077f1c169965f5991ff58d6ac133c172aa
Bianca-ME <bianca.mgeg@protonmail.com>
2025-05-10 17:34:15 +0300

    Configure registers with bitwise operations to turn on LED

--- Src/main.c
+++ Src/main.c
@@ -8,21 +8,22 @@ int main(void){
 
 	// 1. enable clock for GPIOD peripheral in AHB1ENR
 	// looking at the RCC AHB1ENR peripheral clock enable register we see we must only set the fourth bit (index 3!!)
-	// the mask to change the fourth bit is 0x00000008
-	*pClockControlRegister |= 0x08; // dereference and set
+	// the mask to change the fourth bit (index 3) is 0x00000008
+	*pClockControlRegister |= ( 1 << 3 ); // dereference and set
 	// RCC > AHB1ENR > GPIODEN
 
 	// 2. configure the GPIO mode of the IO pin as output
 	// each pin consumes two bits. we need pin with index 12 (bits 24 and 25). must write 01 there (see under table in reference manual) but first
 	// a. CLEAR bits on position 24 and 25
-	*pPortDmodeRegister &= 0XFCFFFFFF;
+	*pPortDmodeRegister &= ~( 3 << 24 );
+	// shifts binary 3 (0011) to the left on indexes 24 and 25, then performs NOT operator, dereferences and performs AND operator
 	// b. SET 01 on position 24 and 25
-	*pPortDmodeRegister |= 0x01000000;
+	*pPortDmodeRegister |= ( 1 << 24 );
 	// GPIOD > MODER > MODER12
 
 	// 3. set 12th bit of the output data register to make I/O pin 12 as HIGH
 	// the mask is 0x00001000
-	*pGPIOoutputDataRegister |= 0x1000;
+	*pGPIOoutputDataRegister |= ( 1 << 12 );
 	// GPIOD > ODR > ODR12
 
 	while(1);
